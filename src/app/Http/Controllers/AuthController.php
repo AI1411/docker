6560b77282426fbd9745831cb778da9b6593ece6
AI1411 <goethe0719@gmail.com>
2020-04-24 21:43:58 +0900

    jwtとcors

---
+++ src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('JWT', ['except' => ['login', 'signup']]);
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json([
+                'error' => 'Unauthorized'
+            ], 401);
+        }
+        return $this->respondWithToken($token);
+    }
+
+    public function signup(SignupRequest $request)
+    {
+        User::create($request->all());
+
+        return $this->login($request);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json([
+            'message' => 'successfully logged out'
+        ]);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()->name
+        ]);
+    }
+}
